246e64250d8364710f771ece9865d3d8c565af7f
carliecode <callcarliecode@yahoo.co.uk>
2024-12-19 16:53:08 +0100

    Added features - Implemented Tag listings data extraction - Implemented Tag pages data extraction - Included 'Description' into returned data

--- scrape_car_information_advanced.py
+++ scrape_car_information_advanced.py
@@ -1,5 +1,4 @@
-import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, ResultSet, Tag
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -13,14 +12,14 @@ import time
 import os
 import logging
 import logging.config
-import yaml
 import json
+import yaml
 
 # Configure logging
-def setup_logging(config_path='config/logging.yml'):
+def setup_logging(config_path: str = 'config/logging.yml') -> logging.Logger:
     try:
         with open(config_path, 'r') as f:
-            config = yaml.safe_load(f.read())
+            config = json.safe_load(f.read())
             logging.config.dictConfig(config)
     except Exception as e:
         logging.basicConfig(level=logging.DEBUG)
@@ -29,10 +28,10 @@ def setup_logging(config_path='config/logging.yml'):
 
 logger = setup_logging()
 
-def configure_chrome_driver():
+def configure_chrome_driver() -> webdriver.Chrome:
     try:
         options = ChromeOptions()
-        # options.add_argument("--headless")  # Comment this out for debugging
+        options.add_argument("--headless")  # Comment this out for debugging
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--disable-gpu")
@@ -53,7 +52,7 @@ def configure_chrome_driver():
         logger.error(f"Error configuring the Chrome WebDriver: {str(e)}")
         raise
 
-def configure_firefox_driver():
+def configure_firefox_driver() -> webdriver.Firefox:
     try:
         options = FirefoxOptions()
         # options.add_argument("--headless")  # Comment this out for debugging
@@ -75,12 +74,12 @@ def configure_firefox_driver():
         logger.error(f"Error configuring the Firefox WebDriver: {str(e)}")
         raise
 
-def get_driver(browser="chrome"):
+def get_driver(browser: str = "chrome") -> webdriver.Chrome | webdriver.Firefox:
     if browser == "firefox":
         return configure_firefox_driver()
     return configure_chrome_driver()
 
-def scroll_to_bottom(driver):
+def scroll_to_bottom(driver: webdriver.Chrome | webdriver.Firefox) -> None:
     try:
         last_height = driver.execute_script("return document.body.scrollHeight")
         while True:
@@ -94,7 +93,7 @@ def scroll_to_bottom(driver):
     except Exception as e:
         logger.error(f"Error while scrolling: {str(e)}")
 
-def get_car_listings(driver, url, retries=3) -> list:
+def get_car_listings(driver: webdriver.Chrome | webdriver.Firefox, url: str, retries: int = 3) -> ResultSet[Tag]:
     try:
         logger.info(f"Accessing URL: {url}")
         driver.get(url)
@@ -102,8 +101,8 @@ def get_car_listings(driver, url, retries=3) -> list:
 
         soup = BeautifulSoup(driver.page_source, "html.parser")
         listings = soup.select("div.masonry-item a.qa-advert-list-item")
-
         logger.info(f'Loading a total of {len(listings)} car listings from {url}')
+
         return listings
     except Exception as e:
         if retries > 0:
@@ -112,29 +111,51 @@ def get_car_listings(driver, url, retries=3) -> list:
             return get_car_listings(driver, url, retries - 1)
         else:
             logger.error(f"Error while getting car listings: {str(e)}")
-            return []
+            return 
 
-def get_listing_details(driver, listing_url) -> dict:
+def get_listing_details(driver: webdriver.Chrome | webdriver.Firefox, listing: Tag, listing_url: str) -> dict:
+    details = {}
     try:
-        logger.info(f"Accessing listing URL: {listing_url}")
+
+        description = listing.find('div', class_='qa-advert-title')
+        description = description.text.strip() if description else 'NA'
+        location = listing.find('span', class_='b-list-advert__region__text')
+        location = location.text.strip() if location else 'NA'
+        price = listing.find('div', class_='qa-advert-price')
+        price  = price.text.strip() if price else 'NA'
+        details['description'] = description
+        details['location'] = location
+        details['price'] = price
+
+        logger.info(f"Decoding ''{description}'' on page : {listing_url}")
         driver.get(listing_url)  
         time.sleep(random.uniform(2, 5))  
         soup = BeautifulSoup(driver.page_source, "html.parser")
-        details = {}
+
         
+        item_condition = soup.find('span', itemprop='itemCondition')
+        item_condition = item_condition.text.strip() if item_condition else 'NA'
+        trans_type = soup.find('span', itemprop='vehicleTransmission')
+        trans_type = trans_type.text.strip() if trans_type else 'NA'
+        fuel_type = soup.find('span', itemprop='fuelType')
+        fuel_type = fuel_type.text.strip() if fuel_type else 'NA'
+        details['item_condition'] = item_condition
+        details['trans_type'] = trans_type
+        details['fuel_type'] = fuel_type
+
         attributes = soup.select("div.b-advert-attribute")
         for attr in attributes:
             key = attr.select_one("div.b-advert-attribute__key").text.strip()
             value = attr.select_one("div.b-advert-attribute__value").text.strip()
             details[key] = value  
 
-        details['url'] = listing_url        
+        #details['url'] = listing_url        
         return details
     except Exception as e:
         logger.error(f"Error while extracting listing details: {str(e)}")
         return {}
 
-def save_to_json_file(data, filename="data.json"):
+def save_to_json_file(data: list, filename: str = "data.json") -> None:
     try:
         if not isinstance(data, list) or not all(isinstance(item, dict) for item in data):
             raise ValueError("Data must be a list of dictionaries")
@@ -150,7 +171,7 @@ def save_to_json_file(data, filename="data.json"):
     except Exception as e:
         logger.error(f"Error saving data to file: {str(e)}")
 
-def rotate_ip():
+def rotate_ip() -> dict:
     try:
         proxies = [
             'http://8.219.97.248:80',
@@ -168,10 +189,10 @@ def rotate_ip():
         logger.error(f"Error while rotating IP: {str(e)}")
         return {}
 
-def scrape_car_prices():
+def scrape_car_prices() -> None:
     base_url = "https://jiji.ng/cars?page="  # Base URL for pagination
     max_pages = 100  # Set to the number of pages you want to scrape
-    os.system('cls')
+    os.system('clear')
     all_details = []
     try:
         driver = get_driver(browser="chrome")
@@ -182,8 +203,9 @@ def scrape_car_prices():
             if not listings:
                 break 
             for listing in listings:
-                listing_url = 'https://jiji.ng' + listing["href"]
-                details = get_listing_details(driver, listing_url)
+                listing_url = 'https://jiji.ng' + listing['href']
+
+                details = get_listing_details(driver, listing, listing_url)
                 if details:  # Ensure dictionary is not empty
                     all_details.append(details)
                 time.sleep(random.uniform(2, 5))
@@ -202,7 +224,7 @@ def scrape_car_prices():
                 if not listings:
                     break 
                 for listing in listings:
-                    listing_url = 'https://jiji.ng' + listing["href"]
+                    listing_url = 'https://jiji.ng' + listing
                     details = get_listing_details(driver, listing_url)
                     if details:  # Ensure dictionary is not empty
                         all_details.append(details)
